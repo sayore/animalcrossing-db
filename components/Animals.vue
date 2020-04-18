19add29f9aa627805e11bf52c1c6caf526043a67
Benjamin Krippner (The Eden Knight) <bkrippner@yahoo.de>
2020-04-18 16:04:30 +0200

    Base Push

---
+++ components/Animals.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+  comphead
+    input.fw-input(v-model="search")
+    div
+      .tb-entry(v-for="v,k in animaldata.fische" v-if="search.length==0 | v.Name.indexOf(search)!==-1") 
+        .typ Fisch
+        .name {{v.Name}}
+        .price 
+          span {{v.Verkaufspreis}}
+          span.small Sternis
+    ul
+      .tb-entry(v-for="v,k in animaldata.insekten" v-if="search.length==0 | v.Name.indexOf(search)!==-1")
+        .typ Insekt
+        .name {{v.Name}}
+        .price 
+          span {{v.Verkaufspreis}}
+          span.small Sternis
+</template>
+
+<script>
+import * as data from '../data.json';
+
+
+module.exports = {
+
+  data: function () {
+    return {
+      search: "",
+      animaldata: data
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tb-entry {
+  display:flex;
+  flex-direction: row;
+  width:100vw;
+  font-size:4vw;
+}
+.tb-entry  > * {
+  flex:3;
+}
+.tb-entry  > .typ {
+  flex:1;
+}
+.tb-entry  > .price {
+  flex:1;
+}
+.fw-input {
+  width:95vw;
+  display:block;
+  font-size:4vw;border:2px black solid;
+  margin:auto;
+}
+comphead {
+  width:100vw;
+}
+span.small {
+  font-size:2vw;
+}
+</style>
---
+++ index.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  html
+    head
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@500&display=swap" rel="stylesheet">
+    body
+      .site
+        h1 Animal Crossing - Verkaufpreise!
+        animals hey
+</template>
+<script>
+import * as data from './data.json';
+import * as Animals from './components/Animals.vue';
+
+module.exports = {
+  components: {
+    Animals
+  },
+  data: function () {
+    return {
+      greeting: data
+    }
+  }
+}
+</script>
+<style>
+/*! minireset.css v0.0.6 | MIT License | github.com/jgthms/minireset.css */html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select,textarea{margin:0}html{box-sizing:border-box}*,*::before,*::after{box-sizing:inherit}img,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}td:not([align]),th:not([align]){text-align:left}
+* {
+  font-family: 'Baloo Bhai 2', cursive;overflow-x: hidden;
+}
+p {
+  font-size: 2em;
+  text-align: center;
+}
+h1 {
+  text-align: center;
+  font-size:3vw;
+}
+</style>
